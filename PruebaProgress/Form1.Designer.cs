cb6a341fdb14188053650eda2d629308e63b6b3b
fseve <falcu2345@gmail.com>
2016-10-18 15:53:13 -0500

    1810

--- PruebaProgress/Form1.Designer.cs
+++ PruebaProgress/Form1.Designer.cs
@@ -29,11 +29,76 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.prog1 = new System.Windows.Forms.ProgressBar();
+            this.btn = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // prog1
+            // 
+            this.prog1.Location = new System.Drawing.Point(110, 149);
+            this.prog1.Name = "prog1";
+            this.prog1.Size = new System.Drawing.Size(100, 23);
+            this.prog1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.prog1.TabIndex = 0;
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(216, 149);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(75, 23);
+            this.btn.TabIndex = 1;
+            this.btn.Text = "Click";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(175, 52);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(0, 13);
+            this.lbl1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(224, 207);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(0, 13);
+            this.lbl2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 427);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.prog1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ProgressBar prog1;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lbl2;
     }
 }
 
--- PruebaProgress/Form1.cs
+++ PruebaProgress/Form1.cs
@@ -12,9 +12,44 @@ namespace PruebaProgress
 {
     public partial class Form1 : Form
     {
+
+        TimeSpan tiempo;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tiempo = TimeSpan.Parse("00:00:30");
+
+            /*prog1.Minimum = 0;
+            prog1.Maximum = 100;
+            prog1.Step = 10;                    */
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            //timer1.Enabled = true;
+            timer1.Enabled = true;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            /*prog1.PerformStep();
+            lbl1.Text = prog1.Value.ToString();            */
+
+            tiempo = tiempo.Subtract(new TimeSpan(00, 00, 01));
+            lbl2.Text = tiempo.ToString();
+
+            if (tiempo == new TimeSpan(00, 00, 00))
+            {
+                timer1.Stop();
+            }
+           
+        }
+        
     }
 }
